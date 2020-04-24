6908e8e99bec1ab020b08f3dc0372500857328ce
Marco <marco.sinatra@mail.polimi.it>
2020-04-24 10:58:02 +0200

    Added the third project together with interface.c, interface.h and variable_definition.h files (all of them are endowed with useful commments)

---
+++ AY1920_II_HW_05_PROJ_3.cydsn/main.c
@@ -0,0 +1,190 @@
+/**
+* 
+* \file main.c
+*
+* \Author Marco Sinatra
+*/
+
+// Include required header files
+#include "I2C_Interface.h"
+#include "project.h"
+#include "stdio.h"
+#include "math.h"
+#include "variable_definition.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    I2C_Peripheral_Start();
+    UART_Debug_Start();
+    
+    CyDelay(5); //"The boot procedure is complete about 5 milliseconds after device power-up."
+    
+    // String to print out messages on the UART
+    char message[50];
+
+    // Check which devices are present on the I2C bus
+    for (int i = 0 ; i < 128; i++)
+    {
+        if (I2C_Peripheral_IsDeviceConnected(i))
+        {
+            // print out the address is hex format
+            sprintf(message, "Device 0x%02X is connected\r\n", i);
+            UART_Debug_PutString(message); 
+        }
+        
+    }
+    
+    /******************************************/
+    /*            I2C Reading                 */
+    /******************************************/
+    
+    /* Read WHO AM I REGISTER register */
+    uint8_t who_am_i_reg;
+    ErrorCode error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                                  LIS3DH_WHO_AM_I_REG_ADDR, 
+                                                  &who_am_i_reg);
+    if (error == NO_ERROR)
+    {
+        sprintf(message, "WHO AM I REG: 0x%02X [Expected: 0x33]\r\n", who_am_i_reg);
+        UART_Debug_PutString(message); 
+    }
+    else
+    {
+        UART_Debug_PutString("Error occurred during I2C comm\r\n");   
+    }
+    
+    /*      I2C Reading Status Register       */
+    
+    uint8_t status_register; 
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_STATUS_REG,
+                                        &status_register);
+    
+    if (error == NO_ERROR)
+    {
+        sprintf(message, "STATUS REGISTER: 0x%02X\r\n", status_register);
+        UART_Debug_PutString(message); 
+    }
+    else
+    {
+        UART_Debug_PutString("Error occurred during I2C comm to read status register\r\n");   
+    }
+    
+    /******************************************/
+    /*        Read Control Register 1         */
+    /******************************************/
+    uint8_t ctrl_reg1; 
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG1,
+                                        &ctrl_reg1);
+    
+    if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 1: 0x%02X\r\n", ctrl_reg1);
+        UART_Debug_PutString(message); 
+    }
+    else
+    {
+        UART_Debug_PutString("Error occurred during I2C comm to read control register 1\r\n");   
+    }
+    
+    /******************************************/
+    /*            I2C Writing                 */
+    /******************************************/
+    
+        
+    UART_Debug_PutString("\r\nWriting new values..\r\n");
+    
+    if (ctrl_reg1 != LIS3DH_NORMAL_MODE_CTRL_REG1)
+    {
+        ctrl_reg1 = LIS3DH_NORMAL_MODE_CTRL_REG1;
+    
+        error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+                                             LIS3DH_CTRL_REG1,
+                                             ctrl_reg1);
+    
+        if (error == NO_ERROR)
+        {
+            sprintf(message, "CONTROL REGISTER 1 successfully written as: 0x%02X\r\n", ctrl_reg1);
+            UART_Debug_PutString(message); 
+        }
+        else
+        {
+            UART_Debug_PutString("Error occurred during I2C comm to set control register 1\r\n");   
+        }
+    }
+    
+    /******************************************/
+    /*     Read Control Register 1 again      */
+    /******************************************/
+
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG1,
+                                        &ctrl_reg1);
+    
+    if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 1 after overwrite operation: 0x%02X\r\n", ctrl_reg1);
+        UART_Debug_PutString(message); 
+    }
+    else
+    {
+        UART_Debug_PutString("Error occurred during I2C comm to read control register 1\r\n");   
+    }
+    
+
+    
+    uint8_t ctrl_reg4;
+
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG4,
+                                        &ctrl_reg4);
+    
+    if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 4: 0x%02X\r\n", ctrl_reg4);
+        UART_Debug_PutString(message); 
+    }
+    else
+    {
+        UART_Debug_PutString("Error occurred during I2C comm to read control register4\r\n");   
+    }
+    
+    
+    ctrl_reg4 = LIS3DH_CTRL_REG4_BDU_ACTIVE; // must be changed to the appropriate value
+    
+    error = I2C_Peripheral_WriteRegister(LIS3DH_DEVICE_ADDRESS,
+                                         LIS3DH_CTRL_REG4,
+                                         ctrl_reg4);
+    
+    error = I2C_Peripheral_ReadRegister(LIS3DH_DEVICE_ADDRESS,
+                                        LIS3DH_CTRL_REG4,
+                                        &ctrl_reg4);
+    
+    
+    if (error == NO_ERROR)
+    {
+        sprintf(message, "CONTROL REGISTER 4 after being updated: 0x%02X\r\n", ctrl_reg4);
+        UART_Debug_PutString(message); 
+    }
+    else
+    {
+        UART_Debug_PutString("Error occurred during I2C comm to read control register4\r\n");   
+    }
+    
+    
+    /***************************************************/
+    /*   variable declaration and for(;;) definition)  */
+    /***************************************************/
+    
+
+    for(;;)
+    {
+
+    }
+}
+
+/* [] END OF FILE */
